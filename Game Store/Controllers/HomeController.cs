841d4cbd93111d5e8655326731d21f86fa132080
jonathan-mokhles <jonathanmokhles@gmail.com>
2025-10-16 12:18:40 +0300

    Structured the project and used clean architecture

--- Game Store/Controllers/HomeController.cs
+++ Game Store/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using Game_Store.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Game_Store.Controllers
@@ -23,10 +22,5 @@ namespace Game_Store.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- Game Store/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Game_Store.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
